0ba7a69aaeee5a9b1894908a292593918d541b87
chozza88 <a94557934@gmail.com>
2026-06-16 15:33:43 +0900

    first system config

---
+++ src/main/java/com/zipdabackend/ZipDaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.zipdabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZipDaBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZipDaBackendApplication.class, args);
+    }
+
+}
