b40f4028c8ff7a3fa8f79649187572470eb590d4
jiyoon0114 <greenpkh02@naver.com>
2026-06-17 18:46:37 +0900

    UserRole 수정

--- src/main/java/com/zipdabackend/global/constant/UserRole.java
+++ src/main/java/com/zipdabackend/global/constant/UserRole.java
@@ -1,4 +1,26 @@
 package com.zipdabackend.global.constant;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+@JsonFormat(shape = JsonFormat.Shape.OBJECT)
 public enum UserRole {
+    USER("USER", "일반 사용자"),
+    AGENT("AGENT", "중개사"),
+    ADMIN("ADMIN", "관리자");
+
+    private final String code;
+    private final String description;
+
+    UserRole(String code, String description) {
+        this.code = code;
+        this.description = description;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public String getDescription() {
+        return description;
+    }
 }
