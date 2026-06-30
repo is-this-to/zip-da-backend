95de533c50efa0061b6aa2814cb8ca62ab18f0b7
soo970220 <soo970220@gmail.com>
2026-06-30 15:42:54 +0900

    지윤씨 병합완료

--- src/main/java/com/zipdabackend/domain/bookmark/service/BookmarkService.java
+++ src/main/java/com/zipdabackend/domain/bookmark/service/BookmarkService.java
@@ -8,6 +8,7 @@ import com.zipdabackend.domain.bookmark.response.BookmarkCardResponse;
 import com.zipdabackend.domain.bookmark.response.BookmarkResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -16,6 +17,7 @@ import java.util.List;
 public class BookmarkService {
   private final BookmarkMapper bookmarkMapper;
 
+  @Transactional
   public BookmarkResponse toggleBookmark(
     Long userId, BookmarkCreateRequest bookmarkCreateRequest) {
     Long propertyId = bookmarkCreateRequest.propertyId();
